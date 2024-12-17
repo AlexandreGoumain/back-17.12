858c166caa5c7e225a424168f3956714d9142b82
sensy-gmn <alexandre26goumain@gmail.com>
2024-12-17 16:15:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const PORT = 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+    try {
+        res.status(200).send("Hello World");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
