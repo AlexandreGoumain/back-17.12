832d32a4f71e419b3d9654d2f2074d0f72ec1471
sensy-gmn <alexandre26goumain@gmail.com>
2024-12-18 14:58:33 +0100

    simple

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ app.use(express.json());
 
 const mongoUri = process.env.MONGO_URI;
 
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(mongoUri);
 
 const db = mongoose.connection;
 
